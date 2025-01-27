db7543a3966af20037be90ca7fadd829d5bf31a0
theandrew168 <andrew@shallowbrooksoftware.com>
2025-01-27 09:17:55 -0700

    Use a map for DB client state

--- src/derztunes/db.clj
+++ src/derztunes/db.clj
@@ -4,57 +4,64 @@
             [next.jdbc.date-time :as jdbc.date-time]
             [next.jdbc.result-set :as rs]))
 
-(defn connect! [db-uri]
+(defn- jdbc-client! [db-uri]
   (jdbc.date-time/read-as-instant)
-  (let [ds (jdbc/get-datasource db-uri)]
-    (jdbc/with-options ds {:builder-fn rs/as-kebab-maps})))
+  (-> (jdbc/get-datasource db-uri)
+      (jdbc/with-options {:builder-fn rs/as-kebab-maps})))
 
-(defn list-tracks! [db]
-  (jdbc/execute! db ["SELECT * FROM track ORDER BY path ASC"]))
+(defn connect! [db-uri]
+  (let [client (jdbc-client! db-uri)]
+    {:db/client client}))
 
-(defn search-tracks! [db q]
-  (jdbc/execute! db ["SELECT * FROM track WHERE path ILIKE ? ORDER BY path ASC" (str "%" q "%")]))
+(defn list-tracks! [conn]
+  (jdbc/execute! (:db/client conn) ["SELECT * FROM track ORDER BY path ASC"]))
 
-(defn read-track! [db id]
-  (jdbc/execute-one! db ["SELECT * FROM track WHERE id = ?" id]))
+(defn search-tracks! [conn q]
+  (jdbc/execute! (:db/client conn) ["SELECT * FROM track WHERE path ILIKE ? ORDER BY path ASC" (str "%" q "%")]))
 
-(defn create-track! [db track]
+(defn read-track! [conn id]
+  (jdbc/execute-one! (:db/client conn) ["SELECT * FROM track WHERE id = ?" id]))
+
+(defn create-track! [conn track]
   (println "Syncing track:" (:track/path track))
-  (jdbc/execute! db ["INSERT INTO track
-                        (id, name, path, created_at, updated_at)
-                      VALUES
-                        (?, ?, ?, ?, ?)
-                      ON CONFLICT (path) DO UPDATE SET name = EXCLUDED.name",
-                     (:track/id track)
-                     (:track/name track)
-                     (:track/path track)
-                     (:track/created-at track)
-                     (:track/updated-at track)]))
+  (jdbc/execute!
+   (:db/client conn)
+   ["INSERT INTO track
+      (id, name, path, created_at, updated_at)
+    VALUES
+      (?, ?, ?, ?, ?)
+    ON CONFLICT (path) DO UPDATE SET name = EXCLUDED.name",
+    (:track/id track)
+    (:track/name track)
+    (:track/path track)
+    (:track/created-at track)
+    (:track/updated-at track)]))
 
-(defn update-track! [db track]
-  (jdbc/execute! db ["UPDATE track
-                      SET
-                        name = ?,
-                        path = ?,
-                        signed_url = ?,
-                        signed_url_expires_at = ?,
-                        updated_at = ?
-                      WHERE id = ?",
-                     (:track/name track)
-                     (:track/path track)
-                     (:track/signed-url track)
-                     (:track/signed-url-expires-at track)
-                     (:track/updated-at track)
-                     (:track/id track)]))
+(defn update-track! [conn track]
+  (jdbc/execute!
+   (:db/client conn)
+   ["UPDATE track
+     SET
+       name = ?,
+       path = ?,
+       signed_url = ?,
+       signed_url_expires_at = ?,
+       updated_at = ?
+     WHERE id = ?",
+    (:track/name track)
+    (:track/path track)
+    (:track/signed-url track)
+    (:track/signed-url-expires-at track)
+    (:track/updated-at track)
+    (:track/id track)]))
 
 (comment
 
   (def uri "postgresql://postgres:postgres@localhost:5432/postgres")
-  (def db (connect! uri))
+  (def conn (connect! uri))
 
-  (create-track! db (model/make-track "foo" "/path/to/foo"))
+  (create-track! conn (model/make-track "foo" "/path/to/foo"))
 
-  (def tracks (list-tracks! db))
-  (println "Hello" "world")
+  (def tracks (list-tracks! conn))
 
   :rcf)
