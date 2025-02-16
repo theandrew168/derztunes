6a23bcd367184c47ec4bdc3eaa08e675730a4b33
theandrew168 <andrew@shallowbrooksoftware.com>
2025-02-15 18:05:37 -0600

    Update TODOs

--- src/derztunes/core.clj
+++ src/derztunes/core.clj
@@ -10,11 +10,13 @@
             [derztunes.sync :as sync])
   (:gen-class))
 
+;; TODO: Add track size to the tracks table.
 ;; TODO: Write my own metadata parsers for MP3 files.
 ;; TODO: Write my own metadata parsers for M4A files.
 ;; TODO: Consider rewriting the tracks table as an HTML table.
 ;; TODO: Make the track table sortable by column.
 ;; TODO: Make the track table columns resizable.
+;; TODO: Add data for the bottom metadata section.
 ;; TODO: Use HTMX for searching.
 ;; TODO: Find better tracks for the demo site.
 ;; TODO: Support systemd notifications for successful startups.
